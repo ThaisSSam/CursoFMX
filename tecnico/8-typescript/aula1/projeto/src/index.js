2850358aebdb0201aee4f7f93df59bb3a30d0250
Thais <thais.s.sam@hotmail.com>
2026-03-11 08:06:34 -0300

    mod 9 - aula 1

---
+++ tecnico/8-typescript/aula1/projeto/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nome = "Maria";
+const idade = 22;
+const estudando = true;
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+console.log(`Estudando: ${estudando}`);
+//# sourceMappingURL=index.js.map
---
+++ tecnico/8-typescript/aula1/projeto/src/index.ts
@@ -0,0 +1,6 @@
+const nome: string = "Maria";
+const idade: number = 22;
+const estudando: boolean = true;
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+console.log(`Estudando: ${estudando}`);
